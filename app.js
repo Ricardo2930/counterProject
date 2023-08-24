fc453d7b56d534e07d6a5b73cccb4f5400aa19ca
Ricardo2930 <rhuaytan@gmail.com>
2023-08-24 15:43:34 -0500

    add readme.md

--- app.js
+++ app.js
@@ -7,32 +7,27 @@ const btnIncrementar = document.getElementById("incrementar")
 const btnReiniciar = document.getElementById("reiniciar")
 const btnDecrementar = document.getElementById("decrementar")
 
-
-
-//Usar el evento onclick en el boton de incremnetar
-btnIncrementar.onclick = function () {
+btnIncrementar.addEventListener('click', () => {
     contador++; //aumento el contador en 1
     valor.innerHTML = contador; //agrego el valor aumentado a la const valor
     if(contador>=1){
         valor.style.color = "#00acfc";
     }   
-}
+}) 
 
-//Usar el evento onclick en el boton de reiniciar
-btnReiniciar.onclick = function () {
+btnReiniciar.addEventListener('click', () => {
     contador = 0; //reinicio el contador a 0
     valor.innerHTML = contador; //agrego el valor aumentado a la const valor
     if(contador >= 0){
         valor.style.color = "#00acfc";
     }
-}
+} ) 
 
-//Usar el evento onclick en el boton de reiniciar
-btnDecrementar.onclick = function () {
+btnDecrementar.addEventListener('click', () => {
     contador--; //decremento el contador en 1
     valor.innerHTML = contador; //agrego el valor aumentado a la const valor
     if(contador<0){
         valor.style.color = "#ff0000";
     }
-}
+}) 
 
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Project Contador JS
+<img src='./Captura de pantalla 2023-08-24 152633.png'/>
